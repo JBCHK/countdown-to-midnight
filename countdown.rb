e71e305ae583f10f8c6ebe8e61713aa5acd3410b
Jonathan Block <blockjonathan29@gmail.com>
2021-01-14 11:07:25 -0500

    commit tree 356636e496bc348620984368731ffdc73235522d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
-
-def countdown
+def countdown_with_sleep(counter)
+counter = 10
+while counter >= 1
+    puts "#{counter} SECOND(S)!"
+    sleep 1
+    counter -= 1
+end 
+    return "HAPPY NEW YEAR!"
 end
+
+
+
+def countdown(counter)
+counter = 10
+while counter >= 1
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+end
+    return "HAPPY NEW YEAR!"
+end
+
+
